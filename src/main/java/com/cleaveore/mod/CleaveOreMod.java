fae0fd5c861280235a3f7513337b00f239f1f0d9
Collin Drake <collinlindendrake@gmail.com>
2026-05-28 12:06:55 -0600

    fix: remove legacy shell registration to prevent cobble fallback conflicts

--- src/main/java/com/cleaveore/mod/CleaveOreMod.java
+++ src/main/java/com/cleaveore/mod/CleaveOreMod.java
@@ -1,11 +1,8 @@
 package com.cleaveore.mod;
 
-import com.cleaveore.mod.registry.ModBlocks;
 import com.mojang.logging.LogUtils;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 @Mod(CleaveOreMod.MOD_ID)
@@ -15,9 +12,6 @@ public class CleaveOreMod {
 
     public CleaveOreMod() {
         CleaveOreConfig.load();
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        ModBlocks.BLOCKS.register(modEventBus);
-        ModBlocks.ITEMS.register(modEventBus);
         MinecraftForge.EVENT_BUS.register(new CleaveOreEvents());
         MinecraftForge.EVENT_BUS.register(new CleaveOreTooltipEvents());
         LOGGER.info("CleaveOre (Forge) initialized.");
